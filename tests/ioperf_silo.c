ac92c4c9fd8f2db135c4367f1391431732c6c919
Mark C Miller <miller86@llnl.gov>
2009-12-01 06:17:59 +0000

    more bug fixes to io performance test tools
    git-svn-id: file:///usr/gapps/silo/svn/trunk@131 92378438-2262-0410-827d-edb41c1c42c8

--- tests/ioperf_silo.c
+++ tests/ioperf_silo.c
@@ -27,13 +27,13 @@ static int Write_silo(void *buf, size_t nbytes)
     static int n = 0;
     int dims[3] = {1, 1, 1};
     int status;
-    void *coords[3] = {0, 0, 0};
 
     dims[0] = nbytes / sizeof(double);
-    coords[0] = buf;
     if (!has_mesh)
     {
         char *coordnames[] = {"x"};
+        void *coords[3] = {0, 0, 0};
+        coords[0] = buf;
         has_mesh = 1;
         status = DBPutQuadmesh(dbfile, "mesh", coordnames, coords, dims, 1, DB_DOUBLE, DB_COLLINEAR, 0);
     }
