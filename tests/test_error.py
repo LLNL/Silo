13c4543ed144213b3ab5d1a7d6ffa70e8b893472
Mark C Miller <miller86@llnl.gov>
2020-03-30 23:57:22 +0000

    making python 2/3 compliant
    git-svn-id: file:///usr/gapps/silo/svn/branches/4.10.3RC@748 92378438-2262-0410-827d-edb41c1c42c8

--- tests/test_error.py
+++ tests/test_error.py
@@ -56,33 +56,33 @@ elif os.access("multi_ucd3d.h5",os.R_OK):
     file = "multi_ucd3d.h5"
 
 db = Silo.Open(file)
-print "db = ",db
+print("db = ",db)
 
 try:
-    print "Trying to set a bad directory"
+    print("Trying to set a bad directory")
     db.SetDir("Magrathea")
 except:
-    print "    -- Caught error"
+    print("    -- Caught error")
 
 try:
-    print "Trying to read a bad variable"
+    print("Trying to read a bad variable")
     db.GetVar("Arkleseizure")
 except:
-    print "    -- Caught error"
+    print("    -- Caught error")
 
-print "Closing...."
+print("Closing....")
 db.Close()
 
-print "db = ",db
+print("db = ",db)
 
 try:
-    print "Trying to set a directory on a closed file"
+    print("Trying to set a directory on a closed file")
     db.SetDir("domain0")
 except:
-    print "    -- Caught error"
+    print("    -- Caught error")
 
 try:
-    print "Trying to read a variable on a closed file"
+    print("Trying to read a variable on a closed file")
     db.SetDir("dtime")
 except:
-    print "    -- Caught error"
+    print("    -- Caught error")
