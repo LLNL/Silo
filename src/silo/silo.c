7007369a25b25dc4fcc0075cec8a73a7bc6e11da
Mark C Miller <miller86@llnl.gov>
2020-10-27 17:44:30 +0000

    add error message for HDF5 but not Silo/HDF5
    git-svn-id: file:///usr/gapps/silo/svn/branches/4.10.3RC@816 92378438-2262-0410-827d-edb41c1c42c8

--- src/silo/silo.c
+++ src/silo/silo.c
@@ -184,8 +184,11 @@ PUBLIC char   *_db_err_list[] =
     "re-install Silo. If you do not have an installation\n"
     "of HDF5 already on your sytem, you will also need\n"
     "to obtain HDF5 from www.hdfgroup.org and install it.", /* 33 */
-    "Empty objects not permitted. See DBSetAllowEmptyObjects()." /* 34 */
-    "No more tiny array buffer space for custom object." /* 35 */
+    "Empty objects not permitted. See DBSetAllowEmptyObjects().", /* 34 */
+    "No more tiny array buffer space for custom object.", /* 35 */
+    "Although this appears to be an HDF5 file,\n"
+    "it does not appear to be one produced by Silo\n"
+    "and so cannot be open and read by Silo." /* 36 */
 };
 
 /* Table of contents object count */
