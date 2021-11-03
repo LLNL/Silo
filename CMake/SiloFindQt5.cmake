e9023dfe12c86cf7e43dc73625074abb7c9b57f7
biagas <biagas2@llnl.gov>
2021-11-03 16:19:01 -0700

    Deploy silex's qt dependencies on Windows
    Move find for Qt5 into silex/CMakeLists.txt

--- CMake/SiloFindQt5.cmake
+++ CMake/SiloFindQt5.cmake
@@ -10,21 +10,45 @@ if(DEFINED SILO_QT5_DIR AND EXISTS ${SILO_QT5_DIR})
     set(Qt5_DIR ${SILO_QT5_DIR})
 endif()
 
-find_package(Qt5 COMPONENTS Core Gui Widgets REQUIRED CONFIG)
+find_package(Qt5 COMPONENTS Core Gui Widgets CONFIG)
 
 if(NOT Qt5_FOUND)
-    # should this be fatal error?
     message(WARNING "Could not find Qt5, you may want to try setting SILO_QT5_DIR")
 endif()
 
+
+###
+# set up for qt's way of deploying necessary dependencies
+###
+
+
+
+## MARK:  NOTE: I think similar to below can be done for MACOS using macdeployqt
+
+##
+# Windows
+##
 if(Qt5_FOUND AND WIN32)
-    get_target_property(Qt5Core_DLL Qt5::Core IMPORTED_LOCATION_RELEASE)
-    get_target_property(Qt5Gui_DLL Qt5::Gui IMPORTED_LOCATION_RELEASE)
-    get_target_property(Qt5Widgets_DLL Qt5::Widgets IMPORTED_LOCATION_RELEASE)
-    install(FILES ${Qt5Core_DLL} ${Qt5Gui_DLL} ${Qt5Widget_DLL}
-            DESTINATION bin
-            PERMISSIONS OWNER_READ OWNER_WRITE
-                        GROUP_READ GROUP_WRITE
-                        WORLD_READ) 
+    # the windeployqt target will be executed as a POST build step for SILEX
+    if(TARGET Qt5::qmake AND NOT TARGET Qt5::windeployqt)
+        get_target_property(_qt5_qmake_location Qt5::qmake IMPORTED_LOCATION)
+
+        execute_process(
+            COMMAND "${_qt5_qmake_location}" -query QT_INSTALL_PREFIX
+            RESULT_VARIABLE return_code
+            OUTPUT_VARIABLE qt5_install_prefix
+            OUTPUT_STRIP_TRAILING_WHITESPACE
+        )
+
+        set(imported_location "${qt5_install_prefix}/bin/windeployqt.exe")
+
+        if(EXISTS ${imported_location})
+            add_executable(Qt5::windeployqt IMPORTED)
+
+            set_target_properties(Qt5::windeployqt PROPERTIES
+                IMPORTED_LOCATION ${imported_location}
+            )
+        endif()
+    endif()
 endif()
 
