acdcc5f935a373c1c603d29d7052895b2f8b6ee3
biagas <biagas2@llnl.gov>
2021-11-10 13:17:29 -0800

    Clean up trailing white space in files added for cmake build.

--- tests/CMake/SiloMakeCheckRunner.cmake
+++ tests/CMake/SiloMakeCheckRunner.cmake
@@ -1,13 +1,13 @@
 # Copyright (C) 1994-2021 Lawrence Livermore National Security, LLC.
 # LLNL-CODE-425250.
 # All rights reserved.
-# 
+#
 # This file is part of Silo. For details, see silo.llnl.gov.
-# 
+#
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
 # are met:
-# 
+#
 #    * Redistributions of source code must retain the above copyright
 #      notice, this list of conditions and the disclaimer below.
 #    * Redistributions in binary form must reproduce the above copyright
@@ -17,7 +17,7 @@
 #    * Neither the name of the LLNS/LLNL nor the names of its
 #      contributors may be used to endorse or promote products derived
 #      from this software without specific prior written permission.
-# 
+#
 # THIS SOFTWARE  IS PROVIDED BY  THE COPYRIGHT HOLDERS  AND CONTRIBUTORS
 # "AS  IS" AND  ANY EXPRESS  OR IMPLIED  WARRANTIES, INCLUDING,  BUT NOT
 # LIMITED TO, THE IMPLIED  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
@@ -30,17 +30,17 @@
 # LIABILITY, WHETHER  IN CONTRACT, STRICT LIABILITY,  OR TORT (INCLUDING
 # NEGLIGENCE OR  OTHERWISE) ARISING IN  ANY WAY OUT  OF THE USE  OF THIS
 # SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-# 
+#
 # This work was produced at Lawrence Livermore National Laboratory under
 # Contract No.  DE-AC52-07NA27344 with the DOE.
-# 
+#
 # Neither the  United States Government nor  Lawrence Livermore National
 # Security, LLC nor any of  their employees, makes any warranty, express
 # or  implied,  or  assumes  any  liability or  responsibility  for  the
 # accuracy, completeness,  or usefulness of  any information, apparatus,
 # product, or  process disclosed, or  represents that its use  would not
 # infringe privately-owned rights.
-# 
+#
 # Any reference herein to  any specific commercial products, process, or
 # services by trade name,  trademark, manufacturer or otherwise does not
 # necessarily  constitute or imply  its endorsement,  recommendation, or
@@ -90,9 +90,9 @@ function(silo_add_make_check_runner)
                         ERROR_QUIET
                         RESULT_VARIABLE check_RESULTS)
     if(check_RESULTS EQUAL 0)
-        message("${samcr_NAME} ${test_args} \t\t\t\t okay") 
+        message("${samcr_NAME} ${test_args} \t\t\t\t okay")
     else()
-        message("${samcr_NAME} ${test_args} \t\t\t\t fail") 
+        message("${samcr_NAME} ${test_args} \t\t\t\t fail")
     endif()
 endfunction()
 
