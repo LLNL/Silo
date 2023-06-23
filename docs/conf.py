0e97198d7edf2b097f581010baccecb1626cf10a
Mark C. Miller <miller86@llnl.gov>
2023-06-23 12:55:00 -0700

    fix RTD tables

--- docs/conf.py
+++ docs/conf.py
@@ -6,3 +6,10 @@ if os.environ.get('READTHEDOCS'):
     from subprocess import call
     call(['pip', 'install', 'myst_parser'])
     extensions.append('myst_parser')
+
+# Attempt add_css_file only if the version of Sphinx actually supports it
+def setup(app):
+    if hasattr(app, 'add_css_file'):
+        app.add_css_file('custom.css')
+    elif hasattr(app, 'add_stylesheet'):
+        app.add_stylesheet('custom.css')
